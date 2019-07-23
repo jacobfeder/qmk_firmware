8a479b8a6e57843457840e493cddefd5fd0e4eb3
Benjamin Shanahan <benshanahan1@gmail.com>
2019-07-22 21:50:39 -0700

    replace KC_ESC with KC_GESC: shift + esc = tilde

--- keyboards/circuit/keymaps/ben/keymap.c
+++ keyboards/circuit/keymaps/ben/keymap.c
@@ -30,7 +30,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
 {
     [ _MAIN ] = KC_KEYMAP(
-        ESC,   1,     2,     3,     4,     5,     6,     7,     8,     9,     0,     MINS,  EQL,   BSLS,  GRV,
+        GESC,  1,     2,     3,     4,     5,     6,     7,     8,     9,     0,     MINS,  EQL,   BSLS,  GRV,
         TAB,   Q,     W,     E,     R,     T,     Y,     U,     I,     O,     P,     LBRC,  RBRC,         BSPC,
         TERM,  A,     S,     D,     F,     G,     H,     J,     K,     L,     SCLN,  QUOT,                ENT,
         LSFT,  Z,     X,     C,     V,     B,     N,     M,     COMM,  DOT,   SLSH,                       RSFT,
